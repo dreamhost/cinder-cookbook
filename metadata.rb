3bdb2d04903853f62a025ecc233c2d8539396f6f
Jeremy Hanmer <jeremy.hanmer@dreamhost.com>
2012-12-13 17:51:29 -0800

    paste updates

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "carl.perry@dreamhost.com"
 license          "Apache 2.0"
 description      "The OpenStack Advanced Volume Management service Cinder."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "5.0.0"
+version          "5.0.1"
 
 %w{ ubuntu fedora }.each do |os|
   supports os
